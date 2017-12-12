9c3b98f1b319b5d75d398e0215d030c4998b2bc9
PatrickShannon <PatrickShannon@patricks-mbp.fios-router.home>
2017-12-12 16:48:42 -0500

    Finished Implementations for Retirement class, Completed UI and fxml layout and controller.

--- FinalExam/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExam/src/main/java/pkgApp/controller/RetirementController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import pkgApp.RetirementApp;
+import pkgCore.Retirement;
 
 public class RetirementController implements Initializable {
 
@@ -16,6 +17,20 @@ public class RetirementController implements Initializable {
 	
 	@FXML
 	private TextField txtYearsToWork;
+	@FXML
+	private TextField txtSaveEachMonth;
+	@FXML
+	private TextField txtAnnualReturnWork;
+	@FXML
+	private TextField txtAmountToSve;
+	@FXML
+	private TextField txtYearsRetired;
+	@FXML
+	private TextField txtAnnualReturnRetired;
+	@FXML
+	private TextField txtRequiredIncome;
+	@FXML
+	private TextField txtMonthlySSI;
 	
 
 	public RetirementApp getMainApp() {
@@ -33,15 +48,50 @@ public class RetirementController implements Initializable {
 	@FXML
 	public void btnClear(ActionEvent event) {
 		System.out.println("Clear pressed");
+		this.txtAmountToSve.clear();
+		this.txtAnnualReturnRetired.clear();
+		this.txtAnnualReturnWork.clear();
+		this.txtMonthlySSI.clear();
+		this.txtRequiredIncome.clear();
+		this.txtSaveEachMonth.clear();
+		this.txtYearsRetired.clear();
+		this.txtYearsToWork.clear();
 		
 		//	TODO: Clear all the text inputs
 	}
 	
 	@FXML
 	public void btnCalculate(ActionEvent event) {
+		try {
+			Integer.parseInt(this.txtYearsToWork.getText());
+			Integer.parseInt(this.txtYearsRetired.getText());
+			Double.parseDouble(this.txtAnnualReturnRetired.getText());
+			Double.parseDouble(this.txtAnnualReturnWork.getText());
+			Double.parseDouble(this.txtMonthlySSI.getText());
+			Double.parseDouble(this.txtRequiredIncome.getText());
+			}
+		catch (Exception e) {
+			this.txtAmountToSve.setText("Please only enter numbers in the calculator");
+		}
+		int yearstowork = Integer.parseInt(this.txtYearsToWork.getText());
+		int yearstoretire = Integer.parseInt(this.txtYearsRetired.getText());
 		
-		//	TODO: Call AmountToSave and TotalAmountSaved and populate 
+		double AnnualReturnRetired = Double.parseDouble(this.txtAnnualReturnRetired.getText());
+		double AnnualReturnWork = Double.parseDouble(this.txtAnnualReturnWork.getText());
+		double MonthlySSI = Double.parseDouble(this.txtMonthlySSI.getText());
+		double Requiredincome = Double.parseDouble(this.txtRequiredIncome.getText());
+
+		Retirement retire = new Retirement(yearstowork, AnnualReturnWork, yearstoretire, AnnualReturnRetired, Requiredincome, MonthlySSI);
 		
+		double saveeachmonth = retire.AmountToSave();
+		double amount2save = retire.TotalAmountSaved();
+		
+		this.txtSaveEachMonth.setText( ( Double.toString(saveeachmonth)));
+		this.txtAmountToSve.setText(Double.toString(amount2save));
 	}
 	
 }
+
+
+
+
--- FinalExam/src/main/java/pkgCore/Retirement.java
+++ FinalExam/src/main/java/pkgCore/Retirement.java
@@ -1,5 +1,7 @@
 package pkgCore;
 
+import org.apache.poi.ss.formula.functions.FinanceLib;
+
 public class Retirement {
 
 	private int iYearsToWork;
@@ -9,22 +11,83 @@ public class Retirement {
 	private double dRequiredIncome;
 	private double dMonthlySSI;
 	
+	public Retirement(int iYearsToWork, double dAnnualReturnWorking, int iYearsRetired, double dAnnualReturnRetired,
+			double dRequiredIncome, double dMonthlySSI) {
+		super();
+		this.iYearsToWork = iYearsToWork;
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+		this.iYearsRetired = iYearsRetired;
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+		this.dRequiredIncome = dRequiredIncome;
+		this.dMonthlySSI = dMonthlySSI;
+	}
+
 	//TODO: Build the contructor, getters and setters for the attributes above.
 	
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
+	}
+
 	public double AmountToSave()
 	{
-		//TODO: Determine the amount to save each month based on TotalAmountSaved, YearsToWork
-		//		and Annual return while working
+		double totalmonths = this.iYearsToWork * 12;
+		double permonthreturn = this.dAnnualReturnWorking/12;
+		double amttosave = this.TotalAmountSaved();
+		double monthlysavings = FinanceLib.pmt( permonthreturn,totalmonths , 0, amttosave, false);
 		
-		
-		return 0;
+		return -1 * monthlysavings;
 	}
 	
 	public double TotalAmountSaved()
 	{
-		//	TODO: Determine amount to be saved based on Monthly SSI, Required Income, Annual return during retirement
-		//		and number of years retired.
-		//
-		return 0;
+		double annualpermonth = this.dAnnualReturnRetired / 12;
+		double totalmonths = this.iYearsRetired * 12;
+		double makeupincome = this.dRequiredIncome - this.dMonthlySSI;
+		double totalamtsave = FinanceLib.pv(annualpermonth, totalmonths, makeupincome , 0 , false);   
+		return -1*totalamtsave;
 	}
 }
